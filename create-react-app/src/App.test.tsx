d408feb41d7ea6e4ceaae46f47048a78f29b9498
Ashley Ryan <asryan@vmware.com>
2022-02-16 11:02:49 -0500

    react: upgrade to create-react-app v5
    Signed-off-by: Ashley Ryan <asryan@vmware.com>

--- create-react-app/src/App.test.tsx
+++ create-react-app/src/App.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react';
+import { App } from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+test('renders learn react link and button', async () => {
+  render(<App />);
+  const linkElement = screen.getByText(/clarity in react/i);
   expect(linkElement).toBeInTheDocument();
+
+  const buttonElement = await screen.findByRole('button', { name: 'Show Alert' });
+  expect(buttonElement).toBeInTheDocument();
 });
