222bc96aaed81c6a0e61b93f3bb3eb7349355e98
Ashley Ryan <asryan@vmware.com>
2022-02-16 11:02:49 -0500

    react: upgrade to create-react-app v5
    Signed-off-by: Ashley Ryan <asryan@vmware.com>

--- create-react-app/src/index.tsx
+++ create-react-app/src/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { App } from './App';
+import reportWebVitals from './reportWebVitals';
 
-import 'normalize.css/normalize.css';
+import 'modern-normalize/modern-normalize.css'; // css reset
 import '@cds/core/global.css'; // pre-minified version breaks
-import '@cds/city/css/bundles/default.min.css';
+import '@cds/city/css/bundles/default.min.css'; // load base font
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
