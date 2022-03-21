efa7ba3d7fd4e2dba554697f521d2a25cb23ac00
Cory Rylan <splintercode.cb@gmail.com>
2022-03-21 09:25:30 -0500

    chore: add svelte demo
    Signed-off-by: Cory Rylan <splintercode.cb@gmail.com>

---
+++ svelte/src/App.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import '@cds/core/button/register.js';
+  import '@cds/core/alert/register.js';
+
+  let status = 'success';
+  let show = false;
+
+  function setShow(value) {
+    show = value;
+  }
+</script>
+
+<main cds-layout="vertical gap:lg p:lg">
+  <h1 cds-text="heading">Clarity + Svelte Starter</h1>
+
+  <div cds-layout="horizontal gap:sm">
+    <a cds-text="link" href="https://clarity.design/storybook/core">Clarity Docs</a>
+    <a cds-text="link" href="https://svelte.dev/">Svelte Docs</a>
+  </div>
+
+  <cds-button action="outline" on:click={() => setShow(!show)}>
+    Hello There
+  </cds-button>
+
+  <cds-alert-group hidden={!show} status={status}>
+    <cds-alert on:closeChange={() => setShow(!show)} closable>
+      General Kenobi, you are a bold one...
+    </cds-alert>
+  </cds-alert-group>
+</main>
+
+<style>
+  @import 'modern-normalize/modern-normalize.css';
+  @import '@cds/core/global.min.css';
+  @import '@cds/core/styles/theme.dark.min.css';
+  @import '@cds/city/css/bundles/default.min.css';
+</style>
