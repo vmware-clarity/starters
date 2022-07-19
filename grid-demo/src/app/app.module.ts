631fef9dd566e84bddf56a6228197fd82fb4c7b8
Kevin Buhmann <kbuhmann@vmware.com>
2022-07-19 16:35:59 -0500

    chore(grid-demo): separate external and local imports
    Signed-off-by: Kevin Buhmann <kbuhmann@vmware.com>

--- grid-demo/src/app/app.module.ts
+++ grid-demo/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ClarityModule } from '@clr/angular';
+import { loadCoreIconSet, loadEssentialIconSet } from '@cds/core/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ClarityModule } from '@clr/angular';
-import { loadCoreIconSet, loadEssentialIconSet } from '@cds/core/icon';
 import { AngularAdvancedRowSelectionComponent } from './angular/advanced-row-selection/advanced-row-selection.component';
 import { AngularColumnDomPerformanceComponent } from './angular/column-dom-performance/column-dom-performance.component';
 import { AngularColumnOrderingComponent } from './angular/column-ordering/column-ordering.component';
