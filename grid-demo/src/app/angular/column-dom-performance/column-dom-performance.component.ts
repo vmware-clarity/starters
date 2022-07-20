fad236d6602a50f32054ee10a261848b0147b568
Kevin Buhmann <kbuhmann@vmware.com>
2022-07-20 18:05:30 -0500

    feat(grid-demo): add angular column dom performance demo
    Signed-off-by: Kevin Buhmann <kbuhmann@vmware.com>

--- grid-demo/src/app/angular/column-dom-performance/column-dom-performance.component.ts
+++ grid-demo/src/app/angular/column-dom-performance/column-dom-performance.component.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { Vm, generateVms, columns } from './../../data/vm.generator';
 
 @Component({
   templateUrl: './column-dom-performance.component.html',
   styleUrls: ['./column-dom-performance.component.scss'],
 })
-export class AngularColumnDomPerformanceComponent {}
+export class AngularColumnDomPerformanceComponent {
+  readonly vms: Observable<Vm[]>;
+  readonly columns = columns.map(column => ({ ...column, hidden: false }));
+
+  constructor() {
+    this.vms = generateVms({ pageIndex: 0, pageSize: 1000 }).pipe(map(data => data.vms));
+  }
+}
