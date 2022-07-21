24d8b6141b980a8cfc437bfa510549f3165af49d
Steve Haar <info@stevehaar.com>
2022-07-21 10:45:43 -0500

    feat: add lazy loading rows angular demo
    Signed-off-by: Steve Haar <info@stevehaar.com>

---
+++ grid-demo/src/app/angular/lazy-loading-rows/lazy-load.service.ts
@@ -0,0 +1,55 @@
+import { ListRange } from '@angular/cdk/collections';
+import { Injectable } from '@angular/core';
+import { from, Subject } from 'rxjs';
+import { debounceTime, distinct, map, scan, mergeMap, startWith, switchMap } from 'rxjs/operators';
+import { Vm, generateVms } from './../../data/vm.generator';
+
+const pageSize = 10;
+
+@Injectable()
+export class LazyLoadService {
+  private readonly listRange = new Subject<ListRange>();
+
+  getVms() {
+    return getTotalResults().pipe(
+      switchMap(totalResults =>
+        this.listRange.pipe(
+          debounceTime(250),
+          map(range => getPageIndexesInView(range)),
+          switchMap(pages => from(pages)),
+          distinct(),
+          mergeMap(pageIndex => generateVms({ pageIndex, pageSize }).pipe(map(data => ({ vms: data.vms, pageIndex })))),
+          scan(
+            (allVms: Vm[], data: { vms: Vm[]; pageIndex: number }) => {
+              const before = allVms.slice(0, data.pageIndex * pageSize);
+              const after = allVms.slice(data.pageIndex * pageSize + pageSize);
+              return [...before, ...data.vms, ...after];
+            },
+            Array.from(Array(totalResults)).map(() => null as any as Vm)
+          ),
+          startWith(Array.from(Array(totalResults)).map(() => null as any as Vm)),
+          map(vms => ({
+            vms,
+            totalResults,
+            loadedCount: vms.filter(vm => vm).length,
+          }))
+        )
+      )
+    );
+  }
+
+  updateRange(range: ListRange) {
+    this.listRange.next(range);
+  }
+}
+
+function getPageIndexesInView(range: ListRange) {
+  const from = Math.floor(range.start / pageSize);
+  const to = Math.floor(range.end / pageSize);
+
+  return Array.from(Array(to - from + 1)).map((_, index) => index + from);
+}
+
+function getTotalResults() {
+  return generateVms({ pageIndex: 0, pageSize: 0 }).pipe(map(data => data.totalCount));
+}
--- grid-demo/src/app/angular/lazy-loading-rows/lazy-loading-rows.component.ts
+++ grid-demo/src/app/angular/lazy-loading-rows/lazy-loading-rows.component.ts
@@ -1,7 +1,30 @@
-import { Component } from '@angular/core';
+import { ListRange } from '@angular/cdk/collections';
+import { AfterViewInit, ChangeDetectorRef, Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+import { Vm, columns } from 'src/app/data/vm.generator';
+import { LazyLoadService } from './lazy-load.service';
 
 @Component({
   templateUrl: './lazy-loading-rows.component.html',
   styleUrls: ['./lazy-loading-rows.component.scss'],
+  providers: [LazyLoadService],
 })
-export class AngularLazyLoadingRowsComponent {}
+export class AngularLazyLoadingRowsComponent {
+  readonly columns = columns;
+  readonly data: Observable<{ vms: Vm[]; totalResults: number; loadedCount: number }>;
+  clrDgSelectable = true;
+
+  constructor(private readonly lazyLoadService: LazyLoadService, changeDetectorRef: ChangeDetectorRef) {
+    this.data = this.lazyLoadService.getVms().pipe(
+      tap(() => {
+        changeDetectorRef.detectChanges();
+      })
+    );
+  }
+
+  renderRangeChange($event: ListRange) {
+    this.lazyLoadService.updateRange($event);
+  }
+}
