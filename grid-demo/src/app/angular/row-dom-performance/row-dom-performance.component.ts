d188236f09de63abbce771866fb068de2e8c2440
Kevin Buhmann <kbuhmann@vmware.com>
2022-07-25 17:35:07 -0500

    feat(grid-demo): add angular virtual scroll demo
    Signed-off-by: Kevin Buhmann <kbuhmann@vmware.com>

--- grid-demo/src/app/angular/row-dom-performance/row-dom-performance.component.ts
+++ grid-demo/src/app/angular/row-dom-performance/row-dom-performance.component.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { Vm, generateVms, columns } from './../../data/vm.generator';
 
 @Component({
   templateUrl: './row-dom-performance.component.html',
   styleUrls: ['./row-dom-performance.component.scss'],
 })
-export class AngularRowDomPerformanceComponent {}
+export class AngularRowDomPerformanceComponent {
+  readonly vms: Observable<Vm[]>;
+  readonly columns = columns;
+
+  constructor() {
+    this.vms = generateVms({ pageIndex: 0, pageSize: 10000 }).pipe(map(data => data.vms));
+  }
+}
