8e3659f1b7f24231d91602066baf8989e36c0381
Steve Haar <info@stevehaar.com>
2022-07-27 15:07:42 -0500

    feat: add screen reader columns demo
    Signed-off-by: Steve Haar <info@stevehaar.com>

---
+++ grid-demo/src/app/angular/screen-reader-columns/screen-reader-columns.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { Vm, generateVms, columns } from './../../data/vm.generator';
+
+@Component({
+  templateUrl: './screen-reader-columns.component.html',
+  styleUrls: ['./screen-reader-columns.component.scss'],
+})
+export class AngularScreenReaderColumnsComponent {
+  readonly vms: Observable<Vm[]>;
+  readonly columns = columns;
+
+  constructor() {
+    this.vms = generateVms({ pageIndex: 0, pageSize: 1000 }).pipe(map(data => data.vms));
+  }
+}
