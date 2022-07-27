9b9088a3657aa42f8ebf07b3674a78ff8212de20
Steve Haar <info@stevehaar.com>
2022-07-27 12:59:52 -0500

    fix: add missing browser animations
    Signed-off-by: Steve Haar <info@stevehaar.com>

--- grid-demo/src/app/app.module.ts
+++ grid-demo/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { BrowserModule } from '@angular/platform-browser';
 import { ClarityModule } from '@clr/angular';
@@ -40,7 +41,7 @@ const directives = [CustomClrAdvancedSelectionDirective];
 
 @NgModule({
   declarations: [...components, ...directives],
-  imports: [AppRoutingModule, BrowserModule, CdsModule, ClarityModule, DragDropModule],
+  imports: [AppRoutingModule, BrowserAnimationsModule, BrowserModule, CdsModule, ClarityModule, DragDropModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent],
 })
