228c6eb100b9e7574ce151cf508dca8bb11591c2
Kevin Buhmann <kbuhmann@vmware.com>
2022-07-29 13:58:55 -0500

    fix(grid-demo): add scroll offset to improve arrow up key navigation
    Signed-off-by: Kevin Buhmann <kbuhmann@vmware.com>

---
+++ grid-demo/src/app/angular/fixes/set-active-cell-scroll-fix.directive.ts
@@ -0,0 +1,27 @@
+import { Directive, ElementRef, OnDestroy, OnInit } from '@angular/core';
+import { ClrDatagrid } from '@clr/angular';
+
+@Directive({
+  selector: 'clr-datagrid',
+})
+export class SetActiveCellScrollFixDirective implements OnInit, OnDestroy {
+  private resizeObserver: ResizeObserver | undefined;
+
+  constructor(private readonly datagrid: ClrDatagrid) {}
+
+  ngOnInit() {
+    const datagridElementRef: ElementRef<HTMLElement> = (this.datagrid as any).el;
+    const datagridDivElement = datagridElementRef.nativeElement.querySelector<HTMLElement>('.datagrid')!;
+    const datagridHeaderElement = datagridElementRef.nativeElement.querySelector<HTMLElement>('.datagrid-header')!;
+
+    this.resizeObserver = new ResizeObserver(() => {
+      datagridDivElement.style.scrollPaddingTop = `${datagridHeaderElement.clientHeight}px`;
+    });
+
+    this.resizeObserver.observe(datagridElementRef.nativeElement);
+  }
+
+  ngOnDestroy() {
+    this.resizeObserver?.disconnect();
+  }
+}
