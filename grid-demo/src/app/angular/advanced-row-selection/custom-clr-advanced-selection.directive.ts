cce38e058f883cf86299ad15a45a83b0215b2d35
Kevin Buhmann <kbuhmann@vmware.com>
2022-08-23 16:50:31 -0500

    chore(grid-demo): rename functions in advanced selection directive
    Signed-off-by: Kevin Buhmann <kbuhmann@vmware.com>

--- grid-demo/src/app/angular/advanced-row-selection/custom-clr-advanced-selection.directive.ts
+++ grid-demo/src/app/angular/advanced-row-selection/custom-clr-advanced-selection.directive.ts
@@ -17,29 +17,29 @@ export class CustomClrAdvancedSelectionDirective implements OnInit {
     const target = event.target as HTMLElement;
 
     if (
-      !isSpecialKeyClick(event) &&
-      !isCheckboxClick(target) &&
-      hasDatagridRowParent(target, this.elementRef.nativeElement)
+      !mouseEventIsSpecialKeyClick(event) &&
+      !elementIsCheckbox(target) &&
+      elementHasDatagridRowParent(target, this.elementRef.nativeElement)
     ) {
       this.datagrid.selected = [];
     }
   }
 }
 
-function isSpecialKeyClick(event: MouseEvent) {
+function mouseEventIsSpecialKeyClick(event: MouseEvent) {
   return event.shiftKey || event.ctrlKey || event.metaKey;
 }
 
-function isCheckboxClick(target: HTMLElement | null) {
-  return target?.parentElement?.className === 'clr-checkbox-wrapper';
+function elementIsCheckbox(element: HTMLElement | null) {
+  return element?.parentElement?.className === 'clr-checkbox-wrapper';
 }
 
-function hasDatagridRowParent(target: HTMLElement | null, maxParent: HTMLElement) {
+function elementHasDatagridRowParent(element: HTMLElement | null, maxParent: HTMLElement) {
   const clrRowTagName = 'CLR-DG-ROW';
 
-  while (target && target !== maxParent && target.tagName !== clrRowTagName) {
-    target = target.parentElement;
+  while (element && element !== maxParent && element.tagName !== clrRowTagName) {
+    element = element.parentElement;
   }
 
-  return target?.tagName === clrRowTagName;
+  return element?.tagName === clrRowTagName;
 }
